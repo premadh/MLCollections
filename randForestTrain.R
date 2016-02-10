c09795a8a4129f4daf7c9cf0e5cb462f014ff1e8
premadh <premraj.adhikari@gmail.com>
2016-02-10 10:02:16 +0200

    Commented on the Code

--- randForestTrain.R
+++ randForestTrain.R
@@ -27,7 +27,7 @@ randForestTrain <- function(finMicro){
 
 for (noofrepeats in 1:100){
   print(paste0("Running iteration: ", noofrepeats))
-  k = 5 #Folds
+  k = 5 # Number of Folds in cross validation
   
   # prediction and testset data frames that we add to 
   # with each iteration over the folds
@@ -39,6 +39,7 @@ for (noofrepeats in 1:100){
   progress.bar <- create_progress_bar("text")
   progress.bar$init(k)
   
+  #We will average the auc value and f1 in each iteration, so initialize them to zeros
   aucval <-0
   f1 <-0
   
@@ -47,22 +48,24 @@ for (noofrepeats in 1:100){
   
   for (i in 1:k){
     
+    #subset the training set from the data
     trainingset <- finMicro[!(folds == i), , drop=FALSE]
     testset <- finMicro[(folds == i), , drop=FALSE]  
     
-    #
+    #Generate a formula for different variables in the dataframe
     frmla <- as.formula(paste(colnames(finMicro)[1], paste(colnames(finMicro)[2:ncol(finMicro)], sep = "", 
                                   collapse = " + "), sep = " ~ "))
-    # run a random forest model
+    # train a random forest model
     mymodel <- randomForest(frmla, data=trainingset, node=3, ntree = 100)
     
-    #predict from the random forest model
+    # predict from the trained random forest model
+    # the result is a probility of the sample taking the value 1
     temp <- as.data.frame(predict(mymodel, testset[,-1],type="prob"))
     
-    #define prediction to calculate AUC
+    # define prediction to calculate AUC
     predf <- prediction(temp[,2],as.factor(testset[,1]))
     
-    #calculate auc
+    # calculate auc
     auc.tmp <- performance(predf,"auc");
     aucval  <- aucval + as.numeric(auc.tmp@y.values)
     
@@ -71,8 +74,9 @@ for (noofrepeats in 1:100){
     
     f1 <- f1+max(ftemp@y.values[[1]],na.rm=TRUE)
     
-    #change the probability in the prediction to 0 and 1
+    # change the probability in the prediction to 0 and 1
     prediction <- rbind(prediction, as.data.frame(as.numeric(temp[,2] > 0.5)))
+    
     # append this iteration's test set to the test set copy data frame
     testsetCopy <- rbind(testsetCopy, as.data.frame(testset[,1]))
     
@@ -83,7 +87,7 @@ for (noofrepeats in 1:100){
   result <- as.data.frame(cbind((prediction), testsetCopy[, 1]))
   names(result) <- c("Predicted", "Actual")
 
-  #calculate confusion matrix
+  #calculate confusion matrix using confusion matrix in caret pacakge
   aa <-  confusionMatrix(factor(result[,1],levels=mylevels),
   factor(result[,2],levels=mylevels), positive='1')
 
